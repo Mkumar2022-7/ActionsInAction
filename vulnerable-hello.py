b877b3e69ec266fe45a86b0766538af8cfd19956
Mkumar2022-7 <103212836+Mkumar2022-7@users.noreply.github.com>
2024-08-16 19:50:36 -0700

    Update vulnerable-hello.py

--- vulnerable-hello.py
+++ vulnerable-hello.py
@@ -4,6 +4,6 @@ import os
 if len(sys.argv) > 1:
     user_input = sys.argv[1]
     # Vulnerable to command injection
-    #os.system(f"echo Hello, {user_input}!")
+    os.system(f"echo Hello, {user_input}!")
 else:
     print("Hello, World!")
